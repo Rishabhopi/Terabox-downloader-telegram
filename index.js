8ba4b4db8a84a477387f1665be94894b6f958837
Rishabh <152057079+Rishabhopi@users.noreply.github.com>
2024-04-19 08:59:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ async function main() {
   bot.start(async (ctx) => {
     try {
       ctx.reply(
-        `Hi ${ctx.message.from.first_name},\n\nI can Download Files from Terabox.\n\nMade with ❤️ by @botcodes123\n\nSend any terabox link to download.`,
+        `Hi ${ctx.message.from.first_name},\n\nI can Download Files from Terabox.\n\nMade with ❤️ by @Rishu1286\n\nSend any terabox link to download.`,
         Markup.inlineKeyboard([
-          Markup.button.url(" Channel", "https://t.me/botcodes123"),
-          Markup.button.url("Report bug", "https://t.me/Armanidrisi_bot"),
+          Markup.button.url(" 🥀Channel😇", "https://t.me/Ur_rishu_143"),
+          Markup.button.url("🤝Report bug🥰", "https://t.me/rishu1286"),
         ]),
       );
     } catch (e) {
